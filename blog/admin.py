2782e376a85fcc79d9c54bc198f0ed9468683475
moayed mahmod <moayedeffect@gmail.com>
2024-01-15 23:27:02 +0000

    Finish custom User model

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from blog.models import Tag, Post, Comment
+from blog.models import Tag, Post, Comment, AuthorProfile
 # Register your models here.
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
     list_display = ('slug', 'published_at')
 
+admin.site.register(AuthorProfile)
 admin.site.register(Tag)
 admin.site.register(Post, PostAdmin)
 admin.site.register(Comment)
