95a3e05df474f3387c0a959e97c8304845ce4476
moayed mahmod <moayedeffect@gmail.com>
2024-01-18 14:03:13 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -16,6 +16,11 @@ urlpatterns += [
     path("auth/", include("rest_framework.urls")),
     path("token-auth/", views.obtain_auth_token),
     path('', include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
